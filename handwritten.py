aa9805ceed874d4e1680f54522242562a06aa1fa
papithotti baby <108323924+Krupanissy123@users.noreply.github.com>
2022-07-29 14:42:59 +0530

    Add files via upload

---
+++ handwritten.py
@@ -0,0 +1,5 @@
+import pywhatkit as pw
+pw.text_to_handwriting("Our channel's name is  Coder's Arcade\n"
+                      "It was started on 3rd january , 2021\n"
+                      "MY name is saurav and I am teaching you python.\n"
+                      "Thnak you and Happy Learning !!!")
